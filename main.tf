fe220665057e44327d6c07b9d747116d947c645c
Vuthy <vuthymay38@gmail.com>
2021-02-23 21:16:17 +0100

    switch Off

--- main.tf
+++ main.tf
@@ -106,6 +106,13 @@ resource "aws_security_group" "worker" {
   tags = {
     Name = "devops-catalog"
   }
+  timeouts {
+    delete = "40m"
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster,
+    aws_iam_role_policy_attachment.service,
+  ]
 }
 
 data "aws_availability_zones" "available" {
@@ -122,6 +129,13 @@ resource "aws_subnet" "worker" {
     "Name"                                      = "devops-catalog"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
+  timeouts {
+    delete = "40m"
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster,
+    aws_iam_role_policy_attachment.service,
+  ]
 }
 
 resource "aws_iam_role" "worker" {
